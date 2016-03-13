23c41ee7158338b77447bc20e886b02ba2385d00
Michael Conrad <michael@threepears.com>
2016-03-13 01:40:04 -0600

    Changes to server, testing routes

--- server.js
+++ server.js
@@ -3,12 +3,20 @@
 const express = require("express");
 const app = express();
 
+// install request
+
 const PORT = process.env.PORT || 3000;
 
 // API page with CORS header
-app.get('/api', (req, res) => {
+app.get('/api/:artist/:sig', (req, res) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.send({hello: 'world'});
+  const artist = req.params.artist;
+  const sig = req.params.sig;
+
+  console.log(artist);
+  console.log(sig);
+
+  res.send(artist);
 });
 
 app.listen(PORT, () => {
