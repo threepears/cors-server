36f14a65e474fdf5e91bcc75b39e47a822b3118d
Michael Conrad <michael@threepears.com>
2016-03-13 21:20:14 -0500

    More search result filters: image size, only main albums

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/api/:artist/:sig', (req, res) => {
   console.log(PORT);
   console.log(process.env);
 
-  const url = "http://api.rovicorp.com/search/v2.1/music/search?apikey=" + rovi + "&sig=" + sig + "&query=" + artist + "&entitytype=artist&size=1&include=images,musicbio,discography,videos&formatid=16";
+  const url = "http://api.rovicorp.com/search/v2.1/music/search?apikey=" + rovi + "&sig=" + sig + "&query=" + artist + "&entitytype=artist&size=1&include=images,musicbio,discography,videos&formatid=62&type=main";
 
   console.log(url);
 
