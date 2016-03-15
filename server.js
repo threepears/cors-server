645c17e2854ffe7ece84be2d4bbd3701a860f447
Michael Conrad <michael@threepears.com>
2016-03-15 17:45:52 -0500

    Clean up code, finalize files

--- server.js
+++ server.js
@@ -19,44 +19,35 @@ app.all('*', function(req, res, next) {
 });
 
 
-// Request artist information from Rovi API
+// Request artist bio, photo, and discography from Rovi API
 app.get('/api/:artist/:sig', (req, res) => {
   const artist = req.params.artist;
   const sig = req.params.sig;
-
   const url = "http://api.rovicorp.com/search/v2.1/music/search?apikey=" + rovi + "&sig=" + sig + "&query=" + artist + "&entitytype=artist&size=1&include=images,musicbio,discography,videos&formatid=62&type=main";
-
-  console.log(url);
 
   request.get(url, (err, response, body) => {
     if (err) throw err;
 
-    // res.header('Access-Control-Allow-Origin', '*');
     res.send(JSON.parse(body));
   });
 });
 
 
-// Request artist's video information from Rovi API
+// Request artist's video information from Echonest API
 app.get('/videos/:artist', (req, res) => {
   const artist = req.params.artist;
-
   const url1 = "http://developer.echonest.com/api/v4/artist/search?api_key=" + echo + "&format=json&name=" + artist + "&results=1";
-
-  console.log(url1);
 
   request.get(url1, (err, response, body) => {
     if (err) throw err;
 
     var bodyparse = JSON.parse(body);
     var artistId = bodyparse.response.artists[0].id;
-
     const url2 = "http://developer.echonest.com/api/v4/artist/video?api_key=" + echo + "&id=" + artistId + "&format=json&results=3&start=0";
 
     request.get(url2, (err, response, body) => {
       if (err) throw err;
 
-      console.log(body);
       res.send(JSON.parse(body));
     });
   });
