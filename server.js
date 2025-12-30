eb3de8ce1a1181eb2f5df9e0eb66b2ffaf1a0ef9
Michael Conrad <michael@threepears.com>
2025-12-30 16:40:54 -0500

    Changing code to reflect change to new Audio DB API

--- server.js
+++ server.js
@@ -1,44 +1,81 @@
 "use strict";
 
+const cors = require('cors')
 const express = require("express");
 const app = express();
 
-// install request
+// Install request
 const request = require('request');
 
 const PORT = process.env.PORT || 3000;
-const rovi = process.env.ROVI_APIKEY || '';
-const echo = process.env.ECHONEST_APIKEY || '';
+const API_KEY = process.env.AUDIO_DB_API_KEY || '';
 
+const urlBase = "https://www.theaudiodb.com/api/v2/json/"
+
+const options = {
+    headers: {
+      "X-API-KEY": API_KEY
+    }
+  };
+
+app.use(cors());
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
+    console.log("ADDING HEADERS");
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("User-Agent", "Hoodat/1.0 http://hoodat.threepears.com/");
     res.header("X-Frame-Options", "SAMEORIGIN");
+    console.log("FINISHED ADDING HEADERS")
     next();
 });
 
-
-// Request artist bio, photo, and discography from Rovi API
-app.get('/api/:artist/:sig', (req, res) => {
+// Request basic artist info to get artist id from The Audio DB API
+app.get('/api/search/artist/:artist', (req, res) => {
   const artist = req.params.artist;
-  const sig = req.params.sig;
-  const url = "http://api.rovicorp.com/search/v2.1/music/search?apikey=" + rovi + "&sig=" + sig + "&query=" + artist + "&entitytype=artist&size=1&include=images,musicbio,discography,videos&formatid=62&type=main";
+  const url = urlBase + "search/artist/" + artist
+  console.log("URL", url)
 
-  request.get(url, (err, response, body) => {
+  request.get(url, options, (err, response, body) => {
+    console.log("BODY", body)
     if (err) throw err;
 
     res.send(JSON.parse(body));
   });
 });
 
+// Request artist bio and photo from The Audio DB API
+app.get('/api/lookup/artist/:artistId', (req, res) => {
+  console.log("GETTING READY TO FETCH THAT SHIZZZ")
+  const artistId = req.params.artistId;
+  const url = urlBase + "lookup/artist/" + artistId
+  console.log("FETCH INFO URL", url)
 
-// Request album information from Rovi API
-app.get('/album/:albumId/:sig', (req, res) => {
+  request.get(url, options, (err, response, body) => {
+    console.log("BODY", body)
+    if (err) throw err;
+
+    res.send(JSON.parse(body));
+  });
+});
+
+// Request discography information from The Audio DB API
+app.get('/api/list/discography/:artistId', (req, res) => {
+  const artistId = req.params.artistId;
+  const url = urlBase + "list/discography/" + artistId
+
+  request.get(url, options, (err, response, body) => {
+    if (err) throw err;
+
+    res.send(JSON.parse(body));
+  });
+});
+
+// Request album information from The Audio DB API
+app.get('/album/:albumId', (req, res) => {
   const albumId = req.params.albumId;
-  const sig = req.params.sig;
-  const url = "http://api.rovicorp.com/data/v1.1/album/info?apikey=" + rovi + "&sig=" + sig + "&albumid=" + albumId + "&include=images,tracks";
+  const url = "https://www.theaudiodb.com/api/v2/json/search/artist/" + artist
 
   request.get(url, (err, response, body) => {
     if (err) throw err;
@@ -46,28 +83,6 @@ app.get('/album/:albumId/:sig', (req, res) => {
     res.send(JSON.parse(body));
   });
 });
-
-
-// Request artist's video information from Echonest API
-app.get('/videos/:artist', (req, res) => {
-  const artist = req.params.artist;
-  const url1 = "http://developer.echonest.com/api/v4/artist/search?api_key=" + echo + "&format=json&name=" + artist + "&results=1";
-
-  request.get(url1, (err, response, body) => {
-    if (err) throw err;
-
-    var bodyparse = JSON.parse(body);
-    var artistId = bodyparse.response.artists[0].id;
-    const url2 = "http://developer.echonest.com/api/v4/artist/video?api_key=" + echo + "&id=" + artistId + "&format=json&results=3&start=0";
-
-    request.get(url2, (err, response, body) => {
-      if (err) throw err;
-
-      res.send(JSON.parse(body));
-    });
-  });
-});
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
